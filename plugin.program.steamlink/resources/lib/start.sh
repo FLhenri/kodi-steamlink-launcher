63147f44fd0cc7bd790d475c05fcf562f94042a4
FLhenri <71257968+FLhenri@users.noreply.github.com>
2021-04-19 07:45:23 +0200

    Update start.sh

--- plugin.program.steamlink/resources/lib/start.sh
+++ plugin.program.steamlink/resources/lib/start.sh
@@ -2,7 +2,9 @@
 
 DIRNAME=$( dirname "${BASH_SOURCE[0]}" )
 
-sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
-sudo systemctl stop kodi
+sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh &"
+sleep 5
+
+sudo systemctl stop kodi 
 
 exit
