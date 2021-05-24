a139b770b59577ed0364c20d7e2458965d96ac78
FLhenri <71257968+FLhenri@users.noreply.github.com>
2021-05-24 18:15:38 +0200

    Update heartbeat.sh

--- plugin.program.steamlink/resources/lib/heartbeat.sh
+++ plugin.program.steamlink/resources/lib/heartbeat.sh
@@ -7,6 +7,10 @@ done
 sudo openvt -c 7 -s -f -- su kodi -c "/usr/bin/xinit /bin/sh -c 'xrandr -s 0; /usr/bin/steamlink' -- :0 -quiet -nolisten tcp"
 sleep 5
 
+export DISPLAY=:0
+sleep 1
+sudo xdotool click 1
+
 while pgrep steamlink &>/dev/null; do
   sleep 5
 done
