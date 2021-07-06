bc351d94907bb6ef839d5085fa2aa5a42f36b12c
FLhenri <71257968+FLhenri@users.noreply.github.com>
2021-07-06 22:24:08 +0200

    Bidouille pour automatisation
    Je ne sais pas pourquoi mais le DISPLAY disparait aléatoirement..
    différents stop pour éviter ça et garder l'automatisation totale

--- plugin.program.steamlink/resources/lib/heartbeat.sh
+++ plugin.program.steamlink/resources/lib/heartbeat.sh
@@ -9,7 +9,44 @@ sleep 5
 
 export DISPLAY=:0
 sleep 1
+nom_ecran=$(sudo xdotool search --onlyvisible .)
+test=0
 sudo xdotool click 1
+
+while test $test = 0; do
+  if test $(sudo xdotool search --onlyvisible .); then
+  	if test ! $nom_ecran = $(sudo xdotool search --onlyvisible .); then
+	    test=1
+	  fi
+  else 
+    export DISPLAY=:0
+	fi
+  sleep 5
+done
+
+while test $test = 1; do
+  if test $(sudo xdotool search --onlyvisible .); then
+  	if test $nom_ecran = $(sudo xdotool search --onlyvisible .); then
+      test=2
+	  fi
+  else 
+    export DISPLAY=:0
+  fi
+  sleep 5
+done
+
+while test $test = 2; do
+  if test $(sudo xdotool search --onlyvisible .); then
+	  if test $nom_ecran = $(sudo xdotool search --onlyvisible .); then
+	    sudo xdotool click 1
+	    sudo xdotool key Escape
+	    test=3
+	  fi
+  else 
+    export DISPLAY=:0
+	fi
+  sleep 5
+done
 
 while pgrep steamlink &>/dev/null; do
   sleep 5
